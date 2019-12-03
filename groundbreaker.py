f7fbcc9092d37ed17eacfb9de41eb752e7b7f1f8
wonggary85 <wong.gary.c@gmail.com>
2019-12-03 10:00:43 -0500

    Added existing priorities handling. Fixed issue clearing massive number of existing priorities.

--- groundbreaker.py
+++ groundbreaker.py
@@ -3,10 +3,10 @@ import getpass
 import os
 import getopt
 import sys
-import random
-import keyring
+import keychainz
 import selenium
 import subprocess
+from random import randint
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -17,6 +17,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import WebDriverException
 
 """
 macos install chromedriver with `brew cask install chromedriver`
@@ -27,8 +34,9 @@ windows you're on your own
 
 def initBrowser():
     chrome_options = Options()
-    #if headless:
-        #chrome_options.add_argument('--headless')
+    load_dotenv()
+    if os.getenv('headless') == True:
+        chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--log-level=3')
     chrome_options.add_argument('--enable-file-cookies')
@@ -38,38 +46,32 @@ def initBrowser():
     capabilities['version'] = '10'
     if sys.platform == 'darwin':
         driver_path = "/usr/local/bin/chromedriver"
+    elif os.path.exists('chromedriver.exe'):
+        # Check if the driver happens to be in the same directory (primarily here for windows users)
+        driver_path = os.path.abspath('chromedriver.exe')
     else:
         if os.getenv('driver_path'):
              driver_path = os.getenv('driver_path')
         else:
-            driver_path = input("Path to chromedriver.exe: ").strip(' "\'\t\r\n')
+            # User input driver path and save to .env
+            driver_path = input("Absolute path to chromedriver.exe: ").strip(' "\'\t\r\n')
             file = open('.env', 'a+')
-            file.write('driver_path=%s' % driver_path)
+            file.write(f'\n\ndriver_path={driver_path}')
             file.close()
     browser = webdriver.Chrome(options=chrome_options, desired_capabilities=capabilities, executable_path=driver_path)
     return browser
 
-def insertCreds(url,user,passwd):
-    urlCreds = user + ":" + passwd + "@"
-    url = url[:8] + urlCreds + url[8:]
-    return url
-
 def randomAnswer(inputFile):
+    # Randomly pick an answer
     lines = []
     with open(inputFile, 'r') as file:
         for line in file:
-            lines.append(line)
-    pick = lines[random.randint(0,len(lines)-1)]
-    return pick    
-
-def keychainz():
-    keyring.set_password("groundbreaker", os.getlogin(), getpass.getpass(prompt="Password: "))
-    print("Saved in keyring!\nRestart script.\n")
-    exit()
+            lines.append(line.strip('\n'))
+    pick = lines[randint(0,len(lines)-1)]
+    return pick
 
 def check_Weekly(url, browser, user, passwd):
     page = False
-    #browser = initBrowser()
     browser.get(url)
     if WebDriverWait(browser, 10).until(EC.title_contains("Login Page")):
         WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.NAME, 'pf.username')))
@@ -77,80 +79,133 @@ def check_Weekly(url, browser, user, passwd):
         browser.find_element_by_id('login-button').click()
         WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, 'passwordInput')))
         browser.find_element_by_id('passwordInput').send_keys(passwd)
-        passwd = ""
         browser.find_element_by_id('login-button').click()
+        iframe = browser.find_element_by_tag_name('iframe')
+        browser.switch_to.frame(iframe)
+        phone = Select(browser.find_element_by_name('device'))
+        phone.select_by_value('phone1')
+        browser.find_element_by_class_name('positive.auth-button').click()
         WebDriverWait(browser, 30).until(EC.title_contains("Team Dashboard"))
         browser.get(url)
-    elif WebDriverWait(browser, 10).until(EC.title_contains("My Check-Ins")):
-        pass
-    else:
+    try:
+        WebDriverWait(browser, 10).until(EC.title_contains("My Check-Ins"))
+    except NoSuchElementException:
         print("No Login or Check-in found.")
         exit()
     WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.ID, 'checkinLauncher')))
     button = browser.find_element_by_class_name('button').click()
     return browser
+
+def nextPage():
+    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'nextButton.pageButton')))
+    browser.find_element_by_class_name('nextButton.pageButton').click()
  
 def submit_Weekly(love, loathe, priority, help, browser):
     """
     Randomly set Strength/Performance between 3-5.
     Input text for love/loathe is selected randomly in their respective text files, one response occupies one line.
     """
-    strength = random.randint(2,4)
-    performance = random.randint(2,4)
-    button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "cirating.engagementrating")))
-    browser.find_elements_by_class_name("cirating.engagementrating")[strength].click()
-    browser.find_elements_by_class_name("cirating.valuerating")[performance].click()
-    browser.find_element_by_class_name('nextButton.pageButton').click()
 
-    # Loved responses
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "input.strText")))
-    inputLove = browser.find_element_by_class_name("input.strText")
-    inputLove.clear()
-    #inputLove = browser.find_element_by_class_name("input.strText")
-    #inputLove.send_keys(love)
-    submit_text(love, inputLove)
+    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "cirating.engagementrating")))
+    for strperf in ["cirating.engagementrating", "cirating.valuerating"]:
+        try:
+            browser.find_elements_by_class_name(strperf)[randint(2,4)].click()
+        except NoSuchElementException:
+            print("Engagement/Value selection not found.")
+            exit()
+    nextPage()
 
-    # Loathed responses
-    browser.find_element_by_class_name("input.weakText").clear()
-    inputLoathe = browser.find_element_by_class_name("input.weakText")
-    inputLoathe.clear()
-    #inputLoathe.send_keys(loathe)
-    submit_text(loathe, inputLoathe)
-    browser.find_element_by_class_name('nextButton.pageButton').click()
+    # Submit loved responses
+    inputLove = ['class_name', 'input.strText']
+    submit_textbox(love, inputLove)
 
-    # Priorities
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, 'dlt-goalinput')))
-    inputPri = browser.find_element_by_id('dlt-goalinput')
-    for item in priority:
-        inputPri.send_keys(item.strip('\n'))
-        browser.find_element_by_id('dlt-addgoalbtn').click()
-    browser.find_element_by_class_name('nextButton.pageButton').click()
-    
-    # Help responses
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, 'needinput')))
-    inputNeed = browser.find_element_by_id('needinput')
-    inputNeed.clear()
-    submit_text(help, inputNeed)
-    browser.find_element_by_class_name('button.pageButton.finishButton').click()
-    print("Submitting")
+    # Submit loathed responses
+    inputLoathe = ['class_name', 'input.weakText']
+    submit_textbox(loathe, inputLoathe)
+    nextPage()
 
-def submit_text(responseList, textbox):
+    # Existing priorities
+    # Currently not carrying over previous priorities to avoid having duplicate entries if a randomly selected response is the same
+    try:
+        if browser.find_element_by_id('prev-goals-page').is_displayed():
+            browser.find_element_by_class_name('nextButton.pageButton').click()
+    except NoSuchElementException:
+        # You probably didn't put in priorities last week since this was not found
+        pass
+
+    # Submit priorities
+    if browser.find_elements_by_class_name('icon-trash-bin.goaloption.removegoal'):
+        remove_goals()
+    inputPri = ['id', 'dlt-goalinput']
+    submit_textbox(priority, inputPri)
+    nextPage()
+
+    # Submit help responses
+    inputHelp = ['class_name', 'input.needinput']
+    submit_textbox(help, inputHelp)
+    nosubmit = os.getenv('nosubmit')
+    if nosubmit == True:
+        print("Not submitting")
+        pass
+    else:
+        #browser.find_element_by_class_name('button.pageButton.finishButton').click()
+        print("Submitting")
+
+def submit_textbox(responseList, selection):
+    if selection[0] == 'class_name':
+        textbox = browser.find_element_by_class_name(selection[1])
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, selection[1])))
+    elif selection[0] == 'id':
+        textbox = browser.find_element_by_id(selection[1])
+        WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, selection[1])))
+    textbox.clear()
     for line in responseList:
         textbox.send_keys(line.strip('\n'))
-        if len(responseList) > 1:
+        if selection[1] == 'dlt-goalinput':
+            browser.find_element_by_id('dlt-addgoalbtn').click()
+        elif len(responseList) > 1:
             ActionChains(browser).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.SHIFT).key_up(Keys.ENTER).perform()
+
+def remove_goals():
+    savedpris = browser.find_elements_by_class_name('icon-trash-bin.goaloption.removegoal')
+    while True:
+        for priority in savedpris:
+            try:
+                WebDriverWait(browser, 3).until(EC.element_to_be_clickable((By.CLASS_NAME, 'icon-trash-bin.goaloption.removegoal')))
+                priority.click()
+                element = WebDriverWait(browser, 3).until(EC.element_to_be_clickable((By.CLASS_NAME, 'btn-small.button.msg-ok-btn')))
+                #browser.find_element_by_class_name('btn-small.button.msg-ok-btn').click()
+                element.click()
+            except (StaleElementReferenceException, TimeoutException, ElementNotInteractableException):
+                pass
+            except ElementClickInterceptedException:
+                try:
+                    if browser.find_element_by_class_name('btn-small.button.msg-ok-btn').is_displayed():
+                        element.click()
+                    elif browser.find_element_by_class_name('icon-trash-bin.goaloption.removegoal').is_displayed():
+                        pass
+                except NoSuchElementException:
+                    pass
+        savedpris = browser.find_elements_by_class_name('icon-trash-bin.goaloption.removegoal')
+        if len(savedpris) == 0:
+            break
+        else:
+            continue
 
 def resetSingleUse():
     with open('singleuse.txt', 'w') as singleuse:
         for category in ['love', 'loathe', 'priority', 'help']:
-            singleuse.write("%s:\n" % category)
- 
+            singleuse.write(f"{category}:\n")
+
 def check_quit():
     while True:
         close_browser = input("Close all windows (Y|N)? ")
         if close_browser.lower() in ('y', 'yes'):
-            browser.close()
-            return
+            try:
+                browser.close()
+                break
+            except WebDriverException:
+                break
 
 def printHelp():
     print("-k | --keychain:\tStores your password (Keychain on macOS or Windows Credential Locker on Windows)")
@@ -194,7 +249,7 @@ def main(argv):
     else:
         for file in ['love.txt', 'loathe.txt', 'priority.txt', 'help.txt']:
             if not os.path.exists(file):
-                print("File %s does not exist. Creating %s." % (file, file))
+                print(f"File {file} does not exist. Creating {file}.")
                 tmp = open(file, 'w+')
                 tmp.close()
         print("Populate responses, separated by newline.\nExiting.")
@@ -203,6 +258,10 @@ def main(argv):
     if os.path.exists('singleuse.txt'):
         if not subprocess.getoutput("md5sum singleuse.txt | awk '{print $1}'") == '8810610524679e08dbaa38c96ac4a3af':
             with open('singleuse.txt', 'r') as singleuse:
+                love = []
+                loathe = []
+                priority = []
+                help = []
                 for line in singleuse:
                     category = line.split(':')
                     if len(category[1]) > 2:
@@ -218,6 +277,29 @@ def main(argv):
     else:
         resetSingleUse()
 
+    if os.path.exists('love.txt') and os.path.exists('loathe.txt') and os.path.exists('priority.txt') and os.path.exists('help.txt'):
+        if singleuse == False:
+            love = set()
+            loathe = set()
+            priority = set()
+            help = set()
+            if os.path.getsize('love.txt') > 0:
+                love.add(randomAnswer('love.txt'))
+            if os.path.getsize('loathe.txt') > 0:
+                loathe.add(randomAnswer('loathe.txt'))
+            if os.path.getsize('priority.txt') > 0:
+                priority.add(randomAnswer('priority.txt'))
+            if os.path.getsize('help.txt') > 0:
+                help.add(randomAnswer('help.txt'))
+    else:
+        for file in ['love.txt', 'loathe.txt', 'priority.txt', 'help.txt']:
+            if not os.path.exists(file):
+                print(f"File {file} does not exist. Creating {file}.")
+                tmp = open(file, 'w+')
+                tmp.close()
+        print("Populate responses, separated by newline.\nExiting.")
+        exit()
+
     try:
         opts,args = getopt.getopt(sys.argv[1:], 'khl:d:p:n:',['love=', 'dislike=', 'priority=', 'need='])
     except Exception as err_msg:
@@ -225,7 +307,7 @@ def main(argv):
         exit()
     for opt,arg in opts:
         if opt in ('-k', '--keychain'):
-            keychainz()
+            passwd = keychainz.setCreds(__file__)
         elif opt in ('-h', '--help'):
             printHelp()
             pass
@@ -243,8 +325,8 @@ def main(argv):
                 help = []
                 help.append(arg)
 
-    if keyring.get_password("groundbreaker", user):
-        passwd = keyring.get_password("groundbreaker", user)
+    if keychainz.getCreds(__file__):
+        passwd = keychainz.getCreds(__file__)
     else:
         try:
             passwd = getpass.getpass(prompt="\nPassword: ")
@@ -255,6 +337,7 @@ def main(argv):
     browser = check_Weekly(url, browser, user, passwd)
     submit_Weekly(love, loathe, priority, help, browser)
     if singleuse == True:
+        print("Clearing singleuse.txt.")
         resetSingleUse()
     check_quit()
     exit()
--- readme.md
+++ readme.md
@@ -1,20 +1,34 @@
-# About
+## Setup
 
-Written in python3. Install modules with `pip3 install -r requirements`.
+* Install python3 and python3-pip
+
+* (Optional) Setup virtualenv
+
+* Install python modules with `pip install -r requirements.txt`
+
+* Install [chromedriver](https://chromedriver.chromium.org/)
+
+* Execute `python3 groundbreaker.py` once to create corresponding files to populate responses
+
+* (Optional) Recommended to run `teamspacerschoice.py -c` to create cronjob
+
+* (Optional) Recommended to run `groundbreaker.py -k` to save password in Keychain/Windows Credential Locker
+
+
+## About
 
 This is composed of two scripts, Groundbreaker.py and Teamspacerschoice.py. Groundbreaker can be used without Teamspacerschoice.
 
 Use Groundbreaker to immediately run a single check-in. Use Teamspacerschoice to schedule a single non-recurring check-in.
 
-Requires [chromedriver](https://chromedriver.chromium.org/) to be installed.
+The scripts only have any value to my colleagues, but can be used as reference and adapted to other things.
 
-Currently not compatible with Windows WSL.
 
 ## Groundbreaker.py 
 
 This script will fetch a list of pre-recorded responses in love.txt, loathe.txt, priority.txt, and help.txt, and randomly select a single response to use.
 
-Login credentials can be stored with option '-k' or '--keychain'. The credentials is saved to the native 'Keychain Access' app in macOS ~~or 'Windows Credential Locker' on Windows~~.
+Login credentials can be stored with option '-k' or '--keychain'. The credentials is saved to the native 'Keychain Access' app in macOS or 'Windows Credential Locker' on Windows.
 
 The script can also automatically retireve and use single use responses recorded in singleuse.txt or with CLI arguments. Single use responses take precedence over pre-recorded responses.
 
@@ -22,11 +36,24 @@ The first run will create the necessary files needed to populate responses.
 
 ## Teamspacerschoice.py
 
+Currently this is incompatible with Windows due to `at` and `python-crontab`.
+
 This uses the bash `at` command. You can view jobs with `atq` command, and remove scheduled jobs with `atrm $JobID`.
 
-Teamspacerschoice can be run once to schedule a groundbreaker.py job. The job is a single non-recurring job (executed once). The scheduled job persists across reboots.
+Teamspacerschoice can be run once to schedule a groundbreaker.py job for the work week (Fri-Thurs). The job is a single non-recurring job (executed once). The scheduled job persists across reboots.
 
 Ideal usage is to execute teamspacerschoice.py with the '-c' option, which will create a cron job to run teamspacerschoice.py every Fridays at 12:00 PM--which will schedule the groundbreaker.py job for the week.
 
 Teamspacerschoice schedules jobs only between 10AM-4PM, except on Fridays between Noon and 4PM.
 
+## .env
+
+A '.env' file can be setup with variables for the URL and chromedriver absolute path.
+
+> url=https://example.com
+> driver_path=/absolute/path/to/chromedriver
+
+
+## To Do
+- [x] Groundbreaker: Implement existing priorities handling
+- [ ] Teamspacerschoice: Windows alternative to `at` and `python-crontab`
